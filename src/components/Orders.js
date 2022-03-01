e69ecd0d84bc1b0c137b9500715154ea260bf511
Deepak-29-y <85682413+Deepak-29-y@users.noreply.github.com>
2022-03-01 11:02:47 +0530

    Order setup

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,10 +1,30 @@
-import React from "react";
-import "orders.css";
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
+import "./orders.css";
+import { useStateValue } from "../StateProvider";
 
 function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    db.collection("user")
+      .doc(user?.uid)
+      .collection("orders")
+      .orderBy("created", "desc")
+      .onSnapshot((snapshot) => {
+        setOrders(
+          snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() }))
+        );
+      });
+  }, []);
   return (
     <div className="orders">
       <h1>Your Orders</h1>
+      <div className="orders__order">
+        {orders?.map((order) => {
+          <Order order={order} />;
+        })}
+      </div>
     </div>
   );
 }
